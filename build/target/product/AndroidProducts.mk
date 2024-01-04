dc40d1932731236c4273d7fd477a5c5408081a3c
janak <niraulajanak2019@gmail.com>
2024-01-04 15:53:58 +0545

    vendor: Refactor for EverestOS
    Signed-off-by: janak <niraulajanak2019@gmail.com>

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2021 The LineageOS Project
+# Copyright (C) 2018-2021 The EverestOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,39 +15,39 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gsi_arm.mk \
-    $(LOCAL_DIR)/lineage_gsi_arm64.mk \
-    $(LOCAL_DIR)/lineage_gsi_x86.mk \
-    $(LOCAL_DIR)/lineage_gsi_x86_64.mk \
-    $(LOCAL_DIR)/lineage_gsi_car_arm64.mk \
-    $(LOCAL_DIR)/lineage_gsi_car_x86_64.mk \
-    $(LOCAL_DIR)/lineage_gsi_tv_arm.mk \
-    $(LOCAL_DIR)/lineage_gsi_tv_arm64.mk \
-    $(LOCAL_DIR)/lineage_gsi_tv_x86.mk \
-    $(LOCAL_DIR)/lineage_gsi_tv_x86_64.mk \
-    $(LOCAL_DIR)/lineage_sdk_car_arm64.mk \
-    $(LOCAL_DIR)/lineage_sdk_car_x86_64.mk \
-    $(LOCAL_DIR)/lineage_sdk_phone_arm64.mk \
-    $(LOCAL_DIR)/lineage_sdk_phone_x86.mk \
-    $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk \
-    $(LOCAL_DIR)/lineage_sdk_tv_arm.mk \
-    $(LOCAL_DIR)/lineage_sdk_tv_x86.mk
+    $(LOCAL_DIR)/everest_gsi_arm.mk \
+    $(LOCAL_DIR)/everest_gsi_arm64.mk \
+    $(LOCAL_DIR)/everest_gsi_x86.mk \
+    $(LOCAL_DIR)/everest_gsi_x86_64.mk \
+    $(LOCAL_DIR)/everest_gsi_car_arm64.mk \
+    $(LOCAL_DIR)/everest_gsi_car_x86_64.mk \
+    $(LOCAL_DIR)/everest_gsi_tv_arm.mk \
+    $(LOCAL_DIR)/everest_gsi_tv_arm64.mk \
+    $(LOCAL_DIR)/everest_gsi_tv_x86.mk \
+    $(LOCAL_DIR)/everest_gsi_tv_x86_64.mk \
+    $(LOCAL_DIR)/everest_sdk_car_arm64.mk \
+    $(LOCAL_DIR)/everest_sdk_car_x86_64.mk \
+    $(LOCAL_DIR)/everest_sdk_phone_arm64.mk \
+    $(LOCAL_DIR)/everest_sdk_phone_x86.mk \
+    $(LOCAL_DIR)/everest_sdk_phone_x86_64.mk \
+    $(LOCAL_DIR)/everest_sdk_tv_arm.mk \
+    $(LOCAL_DIR)/everest_sdk_tv_x86.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gsi_arm-userdebug \
-    lineage_gsi_arm64-userdebug \
-    lineage_gsi_x86-userdebug \
-    lineage_gsi_x86_64-userdebug \
-    lineage_gsi_car_arm64-userdebug \
-    lineage_gsi_car_x86_64-userdebug \
-    lineage_gsi_tv_arm-userdebug \
-    lineage_gsi_tv_arm64-userdebug \
-    lineage_gsi_tv_x86-userdebug \
-    lineage_gsi_tv_x86_64-userdebug \
-    lineage_sdk_car_arm64-userdebug \
-    lineage_sdk_car_x86_64-userdebug \
-    lineage_sdk_phone_arm64-userdebug \
-    lineage_sdk_phone_x86-userdebug \
-    lineage_sdk_phone_x86_64-userdebug \
-    lineage_sdk_tv_arm-userdebug \
-    lineage_sdk_tv_x86-userdebug
+    everest_gsi_arm-userdebug \
+    everest_gsi_arm64-userdebug \
+    everest_gsi_x86-userdebug \
+    everest_gsi_x86_64-userdebug \
+    everest_gsi_car_arm64-userdebug \
+    everest_gsi_car_x86_64-userdebug \
+    everest_gsi_tv_arm-userdebug \
+    everest_gsi_tv_arm64-userdebug \
+    everest_gsi_tv_x86-userdebug \
+    everest_gsi_tv_x86_64-userdebug \
+    everest_sdk_car_arm64-userdebug \
+    everest_sdk_car_x86_64-userdebug \
+    everest_sdk_phone_arm64-userdebug \
+    everest_sdk_phone_x86-userdebug \
+    everest_sdk_phone_x86_64-userdebug \
+    everest_sdk_tv_arm-userdebug \
+    everest_sdk_tv_x86-userdebug
